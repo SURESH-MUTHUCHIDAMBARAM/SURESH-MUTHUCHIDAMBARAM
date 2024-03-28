cbbb6697e8d057f3fc0aa0976dbff668f09db85b
Suresh Muthu <suresh49ca@gmail.com>
2024-03-28 07:55:53 +0530

    sample

--- index.js
+++
@@ -1,2 +0,0 @@
-const suresh={firstname:"suresh",lastname:"muthu",
-age:24,sex:"male",education:"master of commerce"}
--- project/cart.js
+++
@@ -1,8 +0,0 @@
-const suresh={
-    fav:["ak","cr7","smith","vk","msd"],
-    teams:["aus","ind","csk","gt","srh"],
-    num:[1,2,3,4,5,6,7,],
-    lanu:["c","c++","java","js","python"]
-};
-let favperson=suresh.find((x)=>x.fav);
-console.log(favperson)
